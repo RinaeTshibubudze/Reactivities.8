5411cf6e9b4474ef8a1a5755312ff9b0e15ef351
Rinae Tshibubudze <vharhee@gmail.com>
2024-03-18 22:41:39 +0200

    Creating our first Query handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,5 +1,8 @@
 
+using Application.Activities;
 using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -8,24 +11,25 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
+        private readonly IMediator _mediator;
+        
+        public ActivitiesController(IMediator mediator)
         {
-            _context = context;
+            _mediator = mediator;
             
         }
-
+       
         [HttpGet] // api/activities
 
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _context.Activities.ToListAsync();
+            return await _mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] // api/activities/id
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
-            return await _context.Activities.FindAsync(id);
+            return Ok();
         }
     }
 }
