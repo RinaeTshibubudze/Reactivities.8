8a0dc1f495ff8f564794e21612c30e1bdcddd210
Rinae Tshibubudze <vharhee@gmail.com>
2024-03-27 10:10:52 +0200

    Handling API Error responses Part 2

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using Application.Core;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,7 +10,16 @@ namespace API.Controllers
     {
         private IMediator _mediator;
 
-        protected IMediator Mediator => _mediator ??= 
-            HttpContext.RequestServices.GetService<IMediator>();        
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess && result.Value != null)
+                return Ok(result.Value); // 200 OK
+            if (result.IsSuccess && result.Value == null)
+                return NotFound();  // 404 Not Found
+            return BadRequest(result.Error); // 400 Bad Request
+        }
     }
 }
